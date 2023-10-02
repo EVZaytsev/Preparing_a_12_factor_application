b71b4a7d76c0d053b52d61a46bb214e0bf449532
EVZaytsev <etiloron@gmail.com>
2023-10-02 23:45:37 +0300

    Add files

--- Visit_schetchik.py
+++ Visit_schetchik.py
@@ -1,5 +1,7 @@
 from flask import Flask
 import redis
+from dotenv import load_dotenv
+
 app = Flask(__name__)
 r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 @app.route('/visit/<pagename>')
