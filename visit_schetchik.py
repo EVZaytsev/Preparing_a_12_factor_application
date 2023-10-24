0e36a0e4bd0d2febbf0b57bccfee608dc8d57ba1
EVZaytsev <92563674+EVZaytsev@users.noreply.github.com>
2023-10-24 17:42:51 +0300

    Update visit_schetchik.py

--- visit_schetchik.py
+++ visit_schetchik.py
@@ -1,15 +1,28 @@
+"""
+visit_schetchik.py
+
+A simple Flask web application to count and display page visits using Redis.
+"""
+
 from flask import Flask
 import redis
 
 app = Flask(__name__)
 r = redis.Redis(host='redis', port=6379)
+
 @app.route('/visit/<pagename>')
 def visit(pagename):
+    """
+    Increment the visit count for a page and return the page name.
+    """
     r.incr(pagename)
     return pagename
-    
+
 @app.route('/show/<pagename>')
 def show(pagename):
+    """
+    Retrieve and display the visit count for a page.
+    """
     result = r.get(pagename)
     return result
 
